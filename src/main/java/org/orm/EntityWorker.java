f939363a093c3561b9faaac96960c128262f9442
ivanv-lab <ivanvozisov33@yandex.ru>
2025-01-09 09:56:41 +0300

    Получил значение аннотации

--- src/main/java/org/orm/EntityWorker.java
+++ src/main/java/org/orm/EntityWorker.java
@@ -1,8 +1,20 @@
 package org.orm;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
 import java.util.Arrays;
+import java.util.List;
 
 public class EntityWorker{
+    Class annotatedClass;
+    Entity annotation;
 
+    public EntityWorker(Class annotatedClass){
+        this.annotatedClass=annotatedClass;
+    }
+
+    public void workWithAnnotation(){
+        annotation= (Entity) annotatedClass.getAnnotation(Entity.class);
+
+    }
 }
--- src/main/java/orm/test/Main.java
+++ src/main/java/orm/test/Main.java
@@ -8,5 +8,7 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         Car car=new Car("asdsad",234);
+        EntityWorker ew=new EntityWorker(car.getClass());
+        ew.workWithAnnotation();
     }
 }
