0e5da6cdf741226c62722305465a96a4ba89cd4b
ivanv-lab <ivanvozisov33@yandex.ru>
2025-01-10 13:27:06 +0300

    Добавить метод нахождения поля, помеченного аннотацией Id и присвоения ему PK

--- src/main/java/orm/test/Main.java
+++ src/main/java/orm/test/Main.java
@@ -7,15 +7,17 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        SQLWorker sqlWorker=new SQLWorker("dfdf");
-        AnnotationWorker entityWorker=new AnnotationWorker(Car.class,
-                sqlWorker);
-        AnnotationWorker entityWorker1=new AnnotationWorker(Employee.class,
-                sqlWorker);
+//        SQLWorker sqlWorker=new SQLWorker("dfdf");
+//        AnnotationWorker entityWorker=new AnnotationWorker(Car.class,
+//                sqlWorker);
+//        AnnotationWorker entityWorker1=new AnnotationWorker(Employee.class,
+//                sqlWorker);
+//
+//        sqlWorker.InitDB();
+//
+//        entityWorker.createTableByEntityAnnotation();
+//        entityWorker1.createTableByEntityAnnotation();
 
-        sqlWorker.InitDB();
 
-        entityWorker.createTableByEntityAnnotation();
-        entityWorker1.createTableByEntityAnnotation();
     }
 }
